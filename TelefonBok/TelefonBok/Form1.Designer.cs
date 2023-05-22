d3a3571a2a3d55d0addd46845544681fc7bd967b
SimonTE21D <112383428+SimonTE21D@users.noreply.github.com>
2023-05-22 10:22:07 +0200

    Add files via upload

---
+++ TelefonBok/TelefonBok/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace TelefonBok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nameTB = new System.Windows.Forms.TextBox();
+            this.pnTB = new System.Windows.Forms.TextBox();
+            this.GmailTB = new System.Windows.Forms.TextBox();
+            this.KlassTB = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Full Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PhoneNumber:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Gmail:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Klass:";
+            // 
+            // nameTB
+            // 
+            this.nameTB.Location = new System.Drawing.Point(134, 2);
+            this.nameTB.Name = "nameTB";
+            this.nameTB.Size = new System.Drawing.Size(302, 26);
+            this.nameTB.TabIndex = 4;
+            // 
+            // pnTB
+            // 
+            this.pnTB.Location = new System.Drawing.Point(134, 38);
+            this.pnTB.Name = "pnTB";
+            this.pnTB.Size = new System.Drawing.Size(302, 26);
+            this.pnTB.TabIndex = 5;
+            // 
+            // GmailTB
+            // 
+            this.GmailTB.Location = new System.Drawing.Point(134, 113);
+            this.GmailTB.Multiline = true;
+            this.GmailTB.Name = "GmailTB";
+            this.GmailTB.Size = new System.Drawing.Size(302, 86);
+            this.GmailTB.TabIndex = 6;
+            // 
+            // KlassTB
+            // 
+            this.KlassTB.Location = new System.Drawing.Point(134, 75);
+            this.KlassTB.Name = "KlassTB";
+            this.KlassTB.Size = new System.Drawing.Size(302, 26);
+            this.KlassTB.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 208);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(527, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 46);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Lägg till";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(527, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(228, 46);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Ta bort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(527, 159);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(228, 46);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Visa listan";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.KlassTB);
+            this.Controls.Add(this.GmailTB);
+            this.Controls.Add(this.pnTB);
+            this.Controls.Add(this.nameTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox nameTB;
+        private System.Windows.Forms.TextBox pnTB;
+        private System.Windows.Forms.TextBox GmailTB;
+        private System.Windows.Forms.TextBox KlassTB;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TelefonBok/TelefonBok/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data. OleDb;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelefonBok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Ger programmet tillgånt till data basen
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.ace.oledb.12.0; Data Source = TelefonBok.accdb";
+            conn.Open();
+
+            //Lägger till data till data bassen från textboxerna
+            OleDbCommand comm = new OleDbCommand();
+            comm.CommandText = "insert into Tabell1(Name, Tel, Gmail, Klass) values('" + nameTB.Text + "', '" + pnTB.Text + "', '" + GmailTB.Text + "', '" + KlassTB.Text + "')";
+            comm.Connection = conn;
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            //Tar bort texten från textboxen efter det har lagits till i data basen
+            nameTB.Text = "";
+            pnTB.Text = "";
+            KlassTB.Text = "";
+            GmailTB.Text = "";
+
+            //Medelar att data överförningen lyckades
+            MessageBox.Show("Data was sucsefully added");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //Ger programmet tillgånt till data basen
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.ace.oledb.12.0; Data Source = TelefonBok.accdb";
+            conn.Open();
+
+            //Tar bort data från databasen där namnet är samma som textboxen
+            OleDbCommand comm = new OleDbCommand();
+            comm.CommandText = "delete from Tabell1 where Name = '" + nameTB.Text + "'";
+            comm.Connection = conn;
+            comm.ExecuteNonQuery();
+            conn.Close();
+
+            //Tar bort texten från textboxen efter det har lagits till i data basen
+            nameTB.Text = "";
+            pnTB.Text = "";
+            KlassTB.Text = "";
+            GmailTB.Text = "";
+
+            //Medelar att data bortagningen lyckades
+            MessageBox.Show("Data was sucsefully removed");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //Ger programmet tillgånt till data basen
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.ace.oledb.12.0; Data Source = TelefonBok.accdb";
+            conn.Open();
+
+
+            //Visar datan i ett data grid
+            OleDbCommand comm = new OleDbCommand();
+            comm.CommandText = "Select * from Tabell1";
+            comm.Connection = conn;
+            OleDbDataAdapter da = new OleDbDataAdapter();
+            da.SelectCommand = comm;
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+
+            conn.Close();
+        }
+    }
+}
